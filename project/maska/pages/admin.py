5cf560a465b1cb066bb799add3633adb37fdd94e
alaaomaribrahim <118222820+alaaomaribrahim@users.noreply.github.com>
2023-08-12 21:13:47 +0300

    qc edit
    this is the edit of qc where i linked the css and html and the pages and made the rest of the back

--- project/maska/pages/admin.py
+++ project/maska/pages/admin.py
@@ -1,15 +1,17 @@
 from django.contrib import admin
-
-# Register your models here.
-from django.contrib import admin
 from.models import Comment
 from.models import Rating
 from.models import Reservation
 from.models import FlightOrder
-
+from.models import CustomPermission
+from.models import CustomUser
 # Register your models here.
 admin.site.register(Comment)
 admin.site.register(Rating)
 admin.site.register(Reservation)
 admin.site.register(FlightOrder)
+admin.site.register(CustomPermission)
+admin.site.register(CustomUser)
+
+
 
--- project/maska/pages/views.py
+++ project/maska/pages/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, redirect
 from .models import Comment
-from .forms import RatingForm
+from .forms import RatingForm # Import the RatingForm from forms.py
 from .forms import Rating
 from .models import Reservation
 from .forms import FlightOrderForm,FlightOrder
 from django.db.models import Count
+from django.contrib import messages
 # Create your views here.
 
 def save_comment(request):
@@ -16,11 +17,9 @@ def save_comment(request):
         # Save the comment to the database
         Comment.objects.create(name=name, comment=comment_text)
         
-        # Redirect to the comments display page
-        return redirect('show_comments')
-    else:
-        # Render the contact form
-        return render(request, 'pages/contact_form.html')
+    # Render the contact form (regardless of whether the request method is POST or not)
+    return render(request, 'pages/contact_form.html')
+
 
 def show_comments(request):
     # Get all comments from the database
@@ -28,19 +27,19 @@ def show_comments(request):
     return render(request, 'pages/comments_display.html', {'comments': comments})
 
 
-def sidebar_with_comments_view(request):
-    # You can add any additional context variables if needed
-    return render(request, 'pages/sidebar_with_comments.html')
+
 
 def submit_rating(request):
     if request.method == 'POST':
         form = RatingForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('rating_display')
+            return redirect('rating_display')  # Assuming you have a URL named 'rating_display'
     else:
         form = RatingForm()
     return render(request, 'pages/rating_submission.html', {'form': form})
+
+
 
 def rating_display(request):
     ratings = Rating.objects.all()
@@ -54,15 +53,28 @@ def edited_reservations(request):
     edited_count = Reservation.objects.filter(edited=True).count()
     return render(request, 'pages/edited_reservations.html', {'edited_count': edited_count})
 
+def trial(request):
+    # Get the counts for edited and canceled reservations (replace with your actual logic)
+    canceled_count = Reservation.objects.filter(canceled=True).count()
+    edited_count = Reservation.objects.filter(edited=True).count()
+
+    return render(request, 'pages/trial.html', {'edited_count': edited_count,'canceled_count': canceled_count})
+
 def order_flight(request):
     if request.method == 'POST':
         form = FlightOrderForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('most_ordered_flight')
+            messages.success(request, 'Flight ordered successfully.')
+            return redirect('order_flight')  # Redirect to the same page (PRG pattern)
+        else:
+            # If the form is not valid, you can also add an error message
+            messages.error(request, 'There was an error. Please check the form and try again.')
+
     else:
         form = FlightOrderForm()
     return render(request, 'pages/order_flight.html', {'form': form})
+
 
 def most_ordered_flight(request):
     most_ordered_flight = FlightOrder.objects.values('flight_number').annotate(count=Count('flight_number')).order_by('-count').first()

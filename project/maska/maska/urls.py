7f8f543e97d52e2d003dd6b3d1357a6d1f3132da
Mariam-samy <mariamsamyeg@gmail.com>
2023-08-12 11:12:50 +0300

    added the login page with its backend

--- project/maska/maska/urls.py
+++ project/maska/maska/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/' , include("home.urls")),
     path('flight/' , include("flight_options.urls")),
-    path('registeration/' , include('registeration.urls')),
+    path('registeration/' , include('registeration.urls'), name='home'),
 ]
--- project/maska/registeration/urls.py
+++ project/maska/registeration/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path ('login/' , views.render_login , name="render_login"),
+    path ('login/' , views.LoginPage , name="render_login"),
     path ('signup/' , views.render_signup , name="render_signup"),
     # path ('login/post' , views.handle_login_post , name="handle_login_post"),
     path ('signup/post' , views.handle_signup_post , name="handle_signup_post"),

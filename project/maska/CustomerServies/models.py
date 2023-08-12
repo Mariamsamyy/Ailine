eaac2c9680a5150083f59933df01f4e16f273bc2
nariman02o <140006179+nariman02o@users.noreply.github.com>
2023-08-12 23:41:24 +0300

    Update models.py

--- project/maska/CustomerServies/models.py
+++ project/maska/CustomerServies/models.py
@@ -1,3 +1,65 @@
+from typing import AbstractSet
+from django.db import models
+from django.forms import ModelForm
+from django.utils import timezone
+from datetime import datetime
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class Passenger(models.Model):
+    firstname = models.CharField(max_length=20, null=False)
+    lastname = models.CharField( max_length=20, null=False)
+    username = models.CharField(max_length=30, null=False, default=None)
+    email = models.EmailField(null=False)
+    nationalId = models.IntegerField()
+    
+    def __str__(self):
+        return f"id {self.id}, name:{self.firstname} {self.lastname}, email:{self.email}"
+
+class Airport(models.Model):
+    
+    code= models.CharField(max_length=3,null=False)
+    city= models.CharField(max_length=50,null=False)
+    
+    def __str__(self):
+        return self.city
+        
+class Flight(models.Model):
+    origen = models.ForeignKey(Airport,on_delete=models.CASCADE,related_name='Depature', default=None, null=False)
+    destination = models.ForeignKey(Airport,on_delete=models.CASCADE,related_name='to', default=None, null=False)
+    duration = models.FloatField(default=3.0)
+    travile_time = models.DateField(default=datetime.now)
+    BordingTime = models.FloatField(default=0)
+    LandingTime = models.FloatField(default=0)
+    Price = models.IntegerField(default=0)
+    passanger = getattr(Passenger,'firtname','lastname')
+
+    def __str__(self):
+        return f"{self.id}: {self.origen} to {self.destination} Duration: {self.duration} Hours  Travile Date: {self.travile_time} Bording Time: {self.BordingTime}  Landing Time: {self.LandingTime} Price: {self.Price} EGP"
+            
+class Login(models.Model):
+    username = models.CharField(max_length=10)
+    password = models.CharField(max_length=20)
+
+
+class ContactForm(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    message = models.TextField()
+
+    def __str__(self):
+        return f"{self.name}, {self.email}, {self.message}"
+    
+class User(models.Model):
+    firstName=models.CharField(max_length = 30,default="")
+    lastName= models.CharField(max_length = 30,default="")
+    email = models.CharField(max_length = 30)
+    password = models.CharField(max_length = 30) 
+    NationalId=models.CharField(max_length = 30,default="")
+
+
+def __str__(self):
+    return self.email
+    
+
+

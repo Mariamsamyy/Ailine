7eac28576768ad3909bfbe0ed33cf7f6523c45b7
nariman02o <140006179+nariman02o@users.noreply.github.com>
2023-08-12 23:37:22 +0300

    Update admin.py

--- project/maska/CustomerServies/admin.py
+++ project/maska/CustomerServies/admin.py
@@ -1,1 +1,13 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
+from .models import Login,Flight,Airport,ContactForm ,Passenger
+
+admin.site.register(Login)
+#admin.site.register(Flight)
+admin.site.register(Airport)
+admin.site.register(ContactForm)
+admin.site.register(Passenger)
+admin.site.register(Flight)
+#admin.site.register(FlightAdmin)
+
+

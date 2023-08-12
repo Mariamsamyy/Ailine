c6b3919ec484a8557407c35171eae71545e24535
nariman02o <omarnariman2@gmail.com>
2023-08-12 21:36:13 +0300

    customer service front&back,contact-usback, authuntication

---
+++ project/Airline/customerService/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.contrib.admin import ModelAdmin
+from .models import Login,Flight,Airport,ContactForm ,Passenger
+
+admin.site.register(Login)
+#admin.site.register(Flight)
+admin.site.register(Airport)
+admin.site.register(ContactForm)
+admin.site.register(Passenger)
+admin.site.register(Flight)
+#admin.site.register(FlightAdmin)
+
+
+#class FlightAdmin(ModelAdmin):
+
+ #  list_display = ('origin', 'destination', 'id')
+  # readonly_fields = ('origin','destination', 'id')
